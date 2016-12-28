d25d0f59ac622bb4f2669b6222f6c641e0585d39
antoabbot <ahabbot@gmail.com>
2016-12-28 23:22:33 +0000

    pxt add cpp files added

---
+++ extension.ts
@@ -0,0 +1,10 @@
+namespace olney {
+    /**
+     * Help goes here.
+     */
+    //% shim=servo::extfun
+    export function extfun(x: number, y: number) {
+        // Dummy implementation for the simulator.
+        return x - y
+    }
+}
