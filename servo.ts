38845a54c369f9406394d7c25e3ec62a77b496ec
antoabbot <ahabbot@gmail.com>
2016-12-29 19:32:41 +0000

    A working basic Servo driver!

--- servo.ts
+++ servo.ts
@@ -1,39 +1,154 @@
-enum ServoId {
-    S01 = 1,
-    S02 = 2,
-    S03 = 3,
-    S04 = 4,
-    S05 = 5,
-    S06 = 6,
-    S07 = 7,
-    S08 = 8,
-    S09 = 9,
-    S10 = 10,
-    S11 = 11,
-    S12 = 12,
-    S13 = 13,
-    S14 = 14,
-    S15 = 15,
-    S16 = 16,
-} 
+
+
+let PCA9685_MODE1 = 0x0
+let PCA9685_PRESCALE = 0xFE;
+// #define LED0_ON_L 0x6
+// #define LED0_ON_H 0x7
+// #define LED0_OFF_L 0x8
+// #define LED0_OFF_H 0x9
+
+let _i2cAddr = 0x40;
+let LED0_ON_L = 0x6;
 
 /**
  * A test comment
  */
 //% color=270 weigth=10
 namespace olney{
-    
+
+    //% shim=pxtrt::panic
+    function panic(code: number) { }
+
+    function die() { panic(142) }  
+
     /**
-     * Move the servo to an angle between zero and 180 degrees
+     * Initialise the board with a I2C address
      **/
-    //% blockId=olney_servo_moveTo
-    //% block="Move servo %servo| to angle %angle"   
-    export function moveTo(servo: ServoId, angle: number) {
-        let clampedangle = Math.clamp(0, 180, angle)
-        basic.showString("S[")
-        basic.showNumber(servo);
-        basic.showString("]=");
-        basic.showNumber(angle);
-        
+    //% blockId=olney_servo_init
+    //% block="Initialise board on Address %address" 
+    export function init(address: number) {
+        _i2cAddr = address;
+        write8(PCA9685_MODE1, 0x0);
     }
+
+
+    /**
+     * Set the PWM frequency
+     **/
+    //% blockId=olney_servo_setPwmFreq
+    //% block="Set PWM frequency to %freq"   
+    export function setPWMFreq(freq: number) {
+        let prescaleval = 25000000;
+        prescaleval /= 4096;
+        prescaleval /= freq;
+        prescaleval -= 1;
+        let prescale = prescaleval; //Math.Floor(prescaleval + 0.5);
+
+        let oldmode = read8(PCA9685_MODE1);        
+        let newmode = (oldmode & 0x7F) | 0x10; // sleep
+        basic.showNumber(oldmode);
+
+        write8(PCA9685_MODE1, newmode); // go to sleep
+        write8(PCA9685_PRESCALE, prescale); // set the prescaler
+        write8(PCA9685_MODE1, oldmode);
+        control.waitMicros(5000);
+        write8(PCA9685_MODE1, oldmode | 0xa1);
+
+        let finalmode = read8(PCA9685_MODE1);        
+        basic.showNumber(finalmode);
+    }
+
+    /**
+     * Set the PWM value to move the servo
+     **/
+    //% blockId=olney_servo_setPwm
+    //% block="Set PWM for servo %servo: | on %on, | off %off"    
+    export function setPWM(servo: number, on: number, off: number) {
+
+        if (servo < 0 || servo > 15 )
+            die();        
+
+        let buf = pins.createBuffer(5);
+        buf[0] = LED0_ON_L + 4 * servo;
+        buf[1] = on & 0xff;
+        buf[2] = (on>>8) & 0xff;
+        buf[3] = off & 0xff;
+        buf[4] = (off>>8) & 0xff;
+        pins.i2cWriteBuffer(_i2cAddr, buf);
+    }
+
+    /**
+     * Sets pin without having to deal with on/off tick placement and properly handles
+     * a zero value as completely off.  Optional invert parameter supports inverting
+     * the pulse for sinking to ground.  Val should be a value from 0 to 4095 inclusive.
+     **/
+    //% blockId=olney_servo_setPin
+    //% block="Set Pin for servo %servo: | value %val, | Invert? %invert"    
+    export function setPin(num: number, val: number, invert: boolean)
+    {
+    // Clamp value between 0 and 4095 inclusive.
+    val = Math.min(val, 4095);
+    if (invert) {
+        if (val == 0) {
+        // Special value for signal fully on.
+        setPWM(num, 4096, 0);
+        }
+        else if (val == 4095) {
+        // Special value for signal fully off.
+        setPWM(num, 0, 4096);
+        }
+        else {
+        setPWM(num, 0, 4095-val);
+        }
+    }
+    else {
+        if (val == 4095) {
+        // Special value for signal fully on.
+        setPWM(num, 4096, 0);
+        }
+        else if (val == 0) {
+        // Special value for signal fully off.
+        setPWM(num, 0, 4096);
+        }
+        else {
+        setPWM(num, 0, val);
+        }
+    }
+    }    
+        
+    function write8(addr: number, val: number) {
+
+        if (val < 0 || val > 0xff )
+            die();   
+        
+        let buf = pins.createBuffer(2);
+        buf[0] = addr;
+        buf[1] = val;
+        pins.i2cWriteBuffer(_i2cAddr, buf);        
+    }
+
+    function read8(addr: number) {
+        pins.i2cWriteNumber(_i2cAddr, addr, NumberFormat.UInt8BE);
+        let val = pins.i2cReadNumber(_i2cAddr, NumberFormat.UInt8BE);
+        return val;        
+    }
+
+/*
+
+uint8_t Adafruit_PWMServoDriver::read8(uint8_t addr) {
+  WIRE.beginTransmission(_i2caddr);
+  WIRE.write(addr);
+  WIRE.endTransmission();
+
+  WIRE.requestFrom((uint8_t)_i2caddr, (uint8_t)1);
+  return WIRE.read();
+}
+
+void Adafruit_PWMServoDriver::write8(uint8_t addr, uint8_t d) {
+  WIRE.beginTransmission(_i2caddr);
+  WIRE.write(addr);
+  WIRE.write(d);
+  WIRE.endTransmission();
+}
+    */
 }
