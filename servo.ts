8fdbe57fe05797c60862e9601f67b2b2a0e1d462
antoabbot <ahabbot@gmail.com>
2016-12-30 19:03:45 +0000

    ordering documentation and general tidy up

--- servo.ts
+++ servo.ts
@@ -5,10 +5,19 @@ let PCA9685_PRESCALE = 0xFE;
 let _i2cAddr = 0x0;
 let LED0_ON_L = 0x6;
 
+enum boardId
+{
+    //% blockId="id_40" block="0x40"
+    id_0x40 = 0x40,
+    //% blockId="id_41" block="0x41"
+    id_0x41 = 0x41,
+
+}    
+
 /**
  * Adafruit PCA9615 Servo board driver. 
  */
-//% color=270 weigth=10
+//% color=270 weight=10
 namespace servo{
 
     //% shim=pxtrt::panic
@@ -17,23 +26,21 @@ namespace servo{
     function die() { panic(142) }  
 
     /**
-     * Initialise the board with an I2C address
+     * Initialise the board with an I2C address. 
      **/
-    //% blockId=olney_servo_init
+    //% blockId=olney_servo_init weight=100
     //% block="Init Servo board on Address %address" 
-    export function init(address: number = 0x40) {
-        
-        // TODO: Constrain the address, possibly add an enum
+    export function init(address: boardId) {
         _i2cAddr = address;
         write8(PCA9685_MODE1, 0x0);
     }
 
 
     /**
-     * Set the PWM frequency
+     * Set the PWM frequency (typically 60 Hz)
      **/
-    //% blockId=olney_servo_setPwmFreq
-    //% block="Set PWM frequency to %freq"   
+    //% blockId=olney_servo_setPwmFreq weight=50
+    //% block="Set PWM frequency to %freq" Hz  
     export function setPWMFreq(freq: number) {
         // Constrain the frequency
         let prescaleval = 25000000;
@@ -56,16 +63,59 @@ namespace servo{
         //basic.showNumber(finalmode);
     }
 
+
     /**
-     * Set the PWM value to move the servo
+     * Sets pin without having to deal with on/off tick placement and properly handles
+     * a zero value as completely off.  Optional invert parameter supports inverting
+     * the pulse for sinking to ground.  Val should be a value from 0 to 4095 inclusive.
      **/
-    //% blockId=olney_servo_setPwm
-    //% block="Set PWM for servo %servo: | on %on, | off %off"    
+    //% blockId=olney_servo_setPin weight=30
+    //% block="Set servo %servo: | to value %val, | Invert? %invert"    
+    export function setPin(servo: number, val: number, invert: boolean)
+    {
+        if (servo < 0 || servo > 15)
+            return;
+        
+        // Clamp value between 0 and 4095 inclusive.
+        val = Math.clamp(0, 4095, val);
+        if (invert) {
+            if (val == 0) {
+                // Special value for signal fully on.
+                setPWM(servo, 4096, 0);
+            }
+            else if (val == 4095) {
+                // Special value for signal fully off.
+                setPWM(servo, 0, 4096);
+            }
+            else {
+                setPWM(servo, 0, 4095-val);
+            }
+        }
+        else {
+            if (val == 4095) {
+                // Special value for signal fully on.
+                setPWM(servo, 4096, 0);
+            }
+            else if (val == 0) {
+                // Special value for signal fully off.
+                setPWM(servo, 0, 4096);
+            }
+            else {
+                setPWM(servo, 0, val);
+            }
+        }
+    }   
+    
+    /**
+     * Set the PWM value to move the servo. 
+     **/
+    //% blockId=olney_servo_setPwm weight=10
+    //% block="Set PWM for servo %servo: | on= %on, | off= %off"  
+    //% advanced=true
     export function setPWM(servo: number, on: number, off: number) {
 
-        // TODO: Do not die, rather constrain the servo range        
-        if (servo < 0 || servo > 15 )
-            die();        
+        if (servo < 0 || servo > 15)
+            return;
 
         let buf = pins.createBuffer(5);
         buf[0] = LED0_ON_L + 4 * servo;
@@ -75,52 +125,8 @@ namespace servo{
         buf[4] = (off>>8) & 0xff;
         pins.i2cWriteBuffer(_i2cAddr, buf);
     }
-
-    /**
-     * Sets pin without having to deal with on/off tick placement and properly handles
-     * a zero value as completely off.  Optional invert parameter supports inverting
-     * the pulse for sinking to ground.  Val should be a value from 0 to 4095 inclusive.
-     **/
-    //% blockId=olney_servo_setPin
-    //% block="Set Pin for servo %servo: | value %val, | Invert? %invert"    
-    export function setPin(num: number, val: number, invert: boolean)
-    {
-    // Clamp value between 0 and 4095 inclusive.
-    val = Math.min(val, 4095);
-    if (invert) {
-        if (val == 0) {
-        // Special value for signal fully on.
-        setPWM(num, 4096, 0);
-        }
-        else if (val == 4095) {
-        // Special value for signal fully off.
-        setPWM(num, 0, 4096);
-        }
-        else {
-        setPWM(num, 0, 4095-val);
-        }
-    }
-    else {
-        if (val == 4095) {
-        // Special value for signal fully on.
-        setPWM(num, 4096, 0);
-        }
-        else if (val == 0) {
-        // Special value for signal fully off.
-        setPWM(num, 0, 4096);
-        }
-        else {
-        setPWM(num, 0, val);
-        }
-    }
-    }    
         
     function write8(addr: number, val: number) {
-
-        // TODO: Do not die, but rather clamp the range        
-        if (val < 0 || val > 0xff )
-            die();   
-        
         let buf = pins.createBuffer(2);
         buf[0] = addr;
         buf[1] = val;
